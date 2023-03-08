3dffc6ecd7b35d89d2adb092cdff89059352ff5d
FYOTOL <117675112+EGY-Temble@users.noreply.github.com>
2023-03-08 12:39:54 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,341 @@
+
+
+
+
+
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    document.write("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
+for (let i = 0; i < 10000000000; i++) {
+    console.log("HACK")
+}
